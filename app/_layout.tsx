1eeb9da2852511ce727b54dbcb72d86a612d52ac
Sagar <sagarthirumuru@gamil.com>
2024-11-18 23:22:21 +0530

    Biometric added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,15 @@
 import { Slot } from "expo-router";
 import { AuthProvider } from "../context/authContext";
+import { BioAuthProvider } from "@/context/bioAuthContext";
 
 export default function Root() {
   // Set up the auth context and render our layout inside of it.
   return (
-    <AuthProvider>
-      <Slot />
-    </AuthProvider>
+    <BioAuthProvider>
+      <AuthProvider>
+        <Slot />
+      </AuthProvider>
+    </BioAuthProvider>
+
   );
 }
