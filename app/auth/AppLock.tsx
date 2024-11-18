20f9129da3fe45fef53e099028cd3ff0e1fa7e67
Sagar <sagarthirumuru@gamil.com>
2024-11-19 01:40:59 +0530

    feat: add biometry re-authentication on app resume after timeout

--- app/auth/AppLock.tsx
+++ app/auth/AppLock.tsx
@@ -1,10 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { View, Text, Button, StyleSheet, Alert } from "react-native";
 import * as LocalAuthentication from "expo-local-authentication";
 import { BioAuthContext } from "@/context/bioAuthContext";
+import { useRouter } from "expo-router";
 
 const AppLock = () => {
     const { setAuthenticated } = useContext(BioAuthContext);
+    const [isSupported, setIsSupported] = useState(true); //state that hold true if biometric sensor is there in the device 
+    const router = useRouter();
+
+    useEffect(() => {
+
+        const checkForHardware = async () => {
+            const supported = await LocalAuthentication.hasHardwareAsync();
+            const isEnrolled = await LocalAuthentication.isEnrolledAsync();
+            setIsSupported(supported && isEnrolled);
+
+            if (!isSupported) {
+                setAuthenticated(true);
+                router.replace("/"); // if biometic is not support route back to main page
+            }
+        }
+
+        checkForHardware(); // on loading the app check for hardware support for biometry 
+    }, []);
 
     const handleAuthentication = async () => {
         try {
@@ -15,6 +34,12 @@ const AppLock = () => {
 
             if (result.success) {
                 await setAuthenticated(true);
+                // Use replace if there's no back navigation possible
+                if (router.canGoBack()) {
+                    router.back(); // Go to the previous screen
+                } else {
+                    router.replace("/"); // Go to the main screen if no previous screen
+                }
             } else {
                 Alert.alert("Failed", "Authentication failed.");
             }
@@ -22,6 +47,10 @@ const AppLock = () => {
             Alert.alert("Error", "Authentication error occurred.");
         }
     };
+
+    if (!isSupported) {
+        return null;
+    }
 
     return (
         <View style={styles.container}>
